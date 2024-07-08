4fb7b729b866958a85e288e5af18efc96c4481a2
Victoria Adeleke <adeleke-victoria@hotmail.co.uk>
2024-07-08 14:42:37 +0100

    added the API and search engine

---
+++ src/index.js
@@ -0,0 +1,64 @@
+let now = new Date();
+
+let p = document.querySelector("p");
+
+let date = now.getDate();
+
+let hours = now.getHours();
+
+let minutes = now.getMinutes();
+
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+p.innerHTML = `${day}, ${month} ${date}, ${year} | Local Time ${hours}:${minutes}`;
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  let temperatureElement = document.querySelector("#temputure-paragrath-two");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  searchCity(searchInputElement.value);
+}
+
+function searchCity(city) {
+  let apiKey = "7c3280d84e3ff37at9daa2a38ab9o743";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
